3269ab4bdaefed66e37546c953a19eb31f6f8dae
belerweb <belerweb@gmail.com>
2013-06-10 13:31:48 +0800

    init rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OibtTv::Application.config.secret_token = '30146db3847f098efea543acb073141ef77909ea2ca1d0859ab157c45382d2d4e88eb4aceb67036dfa28f947c29ca3a88da1fd1af1dd1f0d05b2403cd0c4baea'
